babf9e487b4ef8488e25ef5d6e7466f5f077d38d
iTsAhyan <mdabdullah265438@gmail.com>
2026-05-23 17:55:06 +0600

    Update videos.js

--- videos.js
+++ videos.js
@@ -1,7 +1,7 @@
 const videoData = [
     { 
         title: "Asian 720p HD Stream", 
-        thumbnail: "https://i.imgur.com/cqKFF8A.png", 
+        thumbnail: "https://transform.gammacdn.com/movies/157812/157812_01/previews/11/31/top_1_1920x1080/157812_01_01.jpg", 
         url: "https://cdn.adultiptv.net/asian.m3u8",
         tags: "Asian, 720p, HD, Streaming, Premium",
         desc: "Watch high quality Asian content in 720p resolution. High-speed streaming experience."
