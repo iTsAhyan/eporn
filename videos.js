217ecfe178fbb94cf325fa342405b847ff2961d5
iTsAhyan <mdabdullah265438@gmail.com>
2026-05-23 18:22:24 +0600

    Update videos.js

--- videos.js
+++ videos.js
@@ -16,14 +16,14 @@ const videoData = [
     { 
         title: "Gangbang 720p HD Stream", 
         thumbnail: "https://thumb-cdn77.xvideos-cdn.com/4b0552dc-49e4-4844-ade0-c9f059ff0639/0/xv_27_p.jpg", 
-        url: "http://cdn.adultiptv.net/gangbang.m3u8",
+        url: "https://live.adultiptv.net/gangbang.m3u8",
         tags: "Gangbang, 720p, HD, Premium, Streaming",
         desc: "Enjoy premium gangbang content streaming at high speed with no lag."
     },
     {
         title: "Hardcore 720p HD Stream", 
         thumbnail: "https://thumbs-gcore.xvideos-cdn.com/9abca360-dcce-42d0-833e-46bef0b2e208/0/xv_13_p.jpg", 
-        url: "http://cdn.adultiptv.net/hardcore.m3u8",
+        url: "https://live.adultiptv.net/hardcore.m3u8",
         tags: "Hardcore, 720p, HD, Premium, Streaming",
         desc: "Enjoy premium hardcore content streaming at high speed with no lag."
     }
