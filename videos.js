d850ecf78107c75eb3e8e591df697b34ff7398fa
iTsAhyan <mdabdullah265438@gmail.com>
2026-05-24 20:39:54 +0600

    Update videos.js

--- videos.js
+++ videos.js
@@ -44,168 +44,168 @@ const videoData = [
     {
     title: "EpornX Live Cams",
     thumbnail: "https://thumb-cdn77.xvideos-cdn.com/15e0c92e-0843-4f5e-be27-37ede398d517/0/xv_27_t.jpg",
-    url: "http://live.adultiptv.net/livecams.m3u8",
+    url: "https://live.adultiptv.net/livecams.m3u8",
     tags: "XXX, Live Cams",
     desc: "Enjoy premium Live Cams content streaming at high speed."
   },
   {
     title: "MILF 1080p HD Stream",
     thumbnail: "https://thumb-cdn77.xvideos-cdn.com/b3fd7f90-4832-41ae-a674-04008c14db1e/0/xv_29_p.jpg",
-    url: "http://live.adultiptv.net/milf.m3u8",
+    url: "https://live.adultiptv.net/milf.m3u8",
     tags: "XXX, MILF",
     desc: "Enjoy premium MILF content streaming at high speed."
   },
   {
     title: "Big Dick 1080p HD Quality",
     thumbnail: "https://thumb-cdn77.xvideos-cdn.com/3824eac3-8d61-437c-b1ce-ccca969634ca/0/xv_28_t.jpg",
-    url: "http://live.adultiptv.net/bigdick.m3u8",
+    url: "https://live.adultiptv.net/bigdick.m3u8",
     tags: "XXX, Big Dick",
     desc: "Enjoy premium Big Dick content streaming at high speed."
   },
   {
     title: "Big Tits 1080p HD TV",
     thumbnail: "https://thumb-cdn77.xvideos-cdn.com/105580d1-4bf5-4eab-b827-ce280291b60d/0/xv_27_t.jpg",
-    url: "http://live.adultiptv.net/bigtits.m3u8",
+    url: "https://live.adultiptv.net/bigtits.m3u8",
     tags: "XXX, Big Tits",
     desc: "Enjoy premium Big Tits content streaming at high speed."
   },
   {
     title: "Fetish 1080p Sex Stream",
     thumbnail: "https://thumb-cdn77.xvideos-cdn.com/5a7a90e7-b43b-4613-8cba-19be7a9007e6/0/xv_8_t.jpg",
-    url: "http://live.adultiptv.net/fetish.m3u8",
+    url: "https://live.adultiptv.net/fetish.m3u8",
     tags: "XXX, Fetish",
     desc: "Enjoy premium Fetish content streaming at high speed."
   },
   {
     title: "Pornstar 1080p HD Quality",
     thumbnail: "https://thumb-cdn77.xvideos-cdn.com/40656673-3e8d-4d61-9b76-8ee98dc3ef24/0/xv_30_t.jpg",
-    url: "http://live.adultiptv.net/pornstar.m3u8",
+    url: "https://live.adultiptv.net/pornstar.m3u8",
     tags: "XXX, Pornstar",
     desc: "Enjoy premium Pornstar content streaming at high speed."
   },
   {
     title: "Big Ass 1080p Fucking Live",
     thumbnail: "https://thumbs-gcore.xvideos-cdn.com/0b1d1641-12bb-4d07-8cf8-2e8c983ace04/0/xv_26_t.jpg",
-    url: "http://live.adultiptv.net/bigass.m3u8",
+    url: "https://live.adultiptv.net/bigass.m3u8",
     tags: "XXX, Big Ass",
     desc: "Enjoy premium Big Ass content streaming at high speed."
   },
   {
     title: "Interracial 1080p HD Quality",
     thumbnail: "https://thumb-cdn77.xvideos-cdn.com/72f281ab-e162-4f52-b008-3286c6bbbcb4/0/xv_16_p.jpg",
-    url: "http://live.adultiptv.net/interracial.m3u8",
+    url: "https://live.adultiptv.net/interracial.m3u8",
     tags: "XXX, Interracial",
     desc: "Enjoy premium Interracial content streaming at high speed."
   },
   {
     title: "Latina 1080p HD Stream",
     thumbnail: "https://thumb-cdn77.xvideos-cdn.com/cb3d147d-99e6-4c70-9e8c-a8bdf449a392/0/xv_19_p.jpg",
-    url: "http://live.adultiptv.net/latina.m3u8",
+    url: "https://live.adultiptv.net/latina.m3u8",
     tags: "XXX, Latina",
     desc: "Enjoy premium Latina content streaming at high speed."
   },
   {
     title: "POV 1080p Hd Sex Video",
     thumbnail: "https://thumb-cdn77.xvideos-cdn.com/700a7584-a362-4a52-a8fd-b8d967cb6605/0/xv_6_p.jpg",
-    url: "http://live.adultiptv.net/pov.m3u8",
+    url: "https://live.adultiptv.net/pov.m3u8",
     tags: "XXX, POV",
     desc: "Enjoy premium POV content streaming at high speed."
   },
   {
     title: "Blowjob 1080p Hd Stream",
     thumbnail: "https://thumb-cdn77.xvideos-cdn.com/35165d85-ad10-43ee-9d3a-526e8dc497a3/0/xv_12_t.jpg",
-    url: "http://live.adultiptv.net/blowjob.m3u8",
+    url: "https://live.adultiptv.net/blowjob.m3u8",
     tags: "XXX, Blowjob",
     desc: "Enjoy premium Blowjob content streaming at high speed."
   },
   {
     title: "Hardcore Live Sex Video",
     thumbnail: "https://thumb-cdn77.xvideos-cdn.com/1e45aae5-9e4a-40fc-bf10-0fd133a67a7f/0/xv_22_t.jpg",
-    url: "http://live.adultiptv.net/hardcore.m3u8",
+    url: "https://live.adultiptv.net/hardcore.m3u8",
     tags: "XXX, Hardcore",
     desc: "Enjoy premium Hardcore content streaming at high speed."
   },
   {
     title: "Cuckold Hot Sex Video",
     thumbnail: "https://thumbs-gcore.xvideos-cdn.com/9abca360-dcce-42d0-833e-46bef0b2e208/0/xv_13_p.jpg",
-    url: "http://live.adultiptv.net/cuckold.m3u8",
+    url: "https://live.adultiptv.net/cuckold.m3u8",
     tags: "XXX, Cuckold",
     desc: "Enjoy premium Cuckold content streaming at high speed."
   },
   {
     title: "Threesome 1080p Hd Live",
     thumbnail: "https://thumb-cdn77.xvideos-cdn.com/2e3930ad-cbc9-4907-9e8f-0a68d3417562/0/xv_21_p.jpg",
-    url: "http://live.adultiptv.net/threesome.m3u8",
+    url: "https://live.adultiptv.net/threesome.m3u8",
     tags: "XXX, Threesome",
     desc: "Enjoy premium Threesome content streaming at high speed."
   },
   {
     title: "Russian Sex Video Live",
     thumbnail: "https://thumb-cdn77.xvideos-cdn.com/f4f8bf76-9439-434c-a708-3c7ee5fb2509/0/xv_1_p.jpg",
-    url: "http://live.adultiptv.net/russian.m3u8",
+    url: "https://live.adultiptv.net/russian.m3u8",
     tags: "XXX, Russian",
     desc: "Enjoy premium Russian content streaming at high speed."
   },
   {
     title: "Lesbian Sex Video Live",
     thumbnail: "https://thumb-cdn77.xvideos-cdn.com/1daa1d6f-9aad-4477-b7e4-cd4725c2e1ab/0/xv_20_p.jpg",
-    url: "http://live.adultiptv.net/lesbian.m3u8",
+    url: "https://live.adultiptv.net/lesbian.m3u8",
     tags: "XXX, Lesbian",
     desc: "Enjoy premium Lesbian content streaming at high speed."
   },
   {
     title: "Rough Sex Video Live",
     thumbnail: "https://thumb-cdn77.xvideos-cdn.com/bf8dfb74-ffcf-4977-b671-fcc5834477ec/0/xv_13_p.jpg",
-    url: "http://live.adultiptv.net/rough.m3u8",
+    url: "https://live.adultiptv.net/rough.m3u8",
     tags: "XXX, Rough",
     desc: "Enjoy premium Rough content streaming at high speed."
   },
   {
     title: "Gangbang Sex Video Live",
     thumbnail: "https://thumb-cdn77.xvideos-cdn.com/e4c72fb6-ba54-454f-9e0f-b08d192db4e6/0/xv_23_p.jpg",
-    url: "http://live.adultiptv.net/gangbang.m3u8",
+    url: "https://live.adultiptv.net/gangbang.m3u8",
     tags: "XXX, Gangbang",
     desc: "Enjoy premium Gangbang content streaming at high speed."
   },
   {
     title: "Anal Sex Video Live",
     thumbnail: "https://thumb-cdn77.xvideos-cdn.com/86c5b646-f836-4b7a-946c-0be81b4db9df/0/xv_26_t.jpg",
-    url: "http://live.adultiptv.net/anal.m3u8",
+    url: "https://live.adultiptv.net/anal.m3u8",
     tags: "XXX, Anal",
     desc: "Enjoy premium Anal content streaming at high speed."
   },
   {
     title: "Compilation Sex Video Live",
     thumbnail: "https://thumb-cdn77.xvideos-cdn.com/c06cf8fc-8e12-458f-9742-4d5756f8a6a5/0/xv_28_p.jpg",
-    url: "http://live.adultiptv.net/compilation.m3u8",
+    url: "https://live.adultiptv.net/compilation.m3u8",
     tags: "XXX, Compilation",
     desc: "Enjoy premium Compilation content streaming at high speed."
   },
   {
     title: "Brunette Sex Video Live",
     thumbnail: "https://thumb-cdn77.xvideos-cdn.com/aa1bda54-e67b-465e-8fda-8c69e199a7d1/3/xv_22_t.jpg",
-    url: "http://live.adultiptv.net/brunette.m3u8",
+    url: "https://live.adultiptv.net/brunette.m3u8",
     tags: "XXX, Brunette",
     desc: "Enjoy premium Brunette content streaming at high speed."
   },
   {
     title: "Blonde Sex Video Live",
     thumbnail: "https://thumb-cdn77.xvideos-cdn.com/0f2c82c4-1f53-4334-b27e-c78b1ec126c5/0/xv_28_p.jpg",
-    url: "http://live.adultiptv.net/blonde.m3u8",
+    url: "https://live.adultiptv.net/blonde.m3u8",
     tags: "XXX, Blonde",
     desc: "Enjoy premium Blonde content streaming at high speed."
   },
   {
     title: "Gay Sex Video Live",
     thumbnail: "https://thumb-cdn77.xvideos-cdn.com/28840331-39ed-474d-a4f9-5e8b6250765c/0/xv_24_p.jpg",
-    url: "http://live.adultiptv.net/gay.m3u8",
+    url: "https://live.adultiptv.net/gay.m3u8",
     tags: "XXX, Gay",
     desc: "Enjoy premium Gay content streaming at high speed."
   },
   {
     title: "Asian Sex Video Live",
     thumbnail: "https://thumb-cdn77.xvideos-cdn.com/7058dfee-dbeb-4187-bbe7-27f4e03959c4/0/xv_20_t.jpg",
-    url: "http://live.adultiptv.net/asian.m3u8",
+    url: "https://live.adultiptv.net/asian.m3u8",
     tags: "XXX, Asian",
     desc: "Enjoy premium Asian content streaming at high speed."
   }
