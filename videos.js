16872831f9a9a1294c26293b7609935f859c51a3
iTsAhyan <mdabdullah265438@gmail.com>
2026-05-24 18:40:35 +0600

    Update videos.js

--- videos.js
+++ videos.js
@@ -41,4 +41,172 @@ const videoData = [
         tags: "Cuckold, 720p, HD, Premium, Streaming",
         desc: "Enjoy premium cuckold content streaming at high speed with no lag."
     },
+    {
+    title: "AdultIPTV.net Live Cams",
+    thumbnail: "https://files.adultiptv.net/adultiptvnet.jpg",
+    url: "http://live.adultiptv.net/livecams.m3u8",
+    tags: "XXX, Live Cams",
+    desc: "Enjoy premium Live Cams content streaming at high speed."
+  },
+  {
+    title: "AdultIPTV.net MILF",
+    thumbnail: "https://files.adultiptv.net/adultiptvnet.jpg",
+    url: "http://live.adultiptv.net/milf.m3u8",
+    tags: "XXX, MILF",
+    desc: "Enjoy premium MILF content streaming at high speed."
+  },
+  {
+    title: "AdultIPTV.net Big Dick",
+    thumbnail: "https://files.adultiptv.net/adultiptvnet.jpg",
+    url: "http://live.adultiptv.net/bigdick.m3u8",
+    tags: "XXX, Big Dick",
+    desc: "Enjoy premium Big Dick content streaming at high speed."
+  },
+  {
+    title: "AdultIPTV.net Big Tits",
+    thumbnail: "https://files.adultiptv.net/adultiptvnet.jpg",
+    url: "http://live.adultiptv.net/bigtits.m3u8",
+    tags: "XXX, Big Tits",
+    desc: "Enjoy premium Big Tits content streaming at high speed."
+  },
+  {
+    title: "AdultIPTV.net Fetish",
+    thumbnail: "https://files.adultiptv.net/adultiptvnet.jpg",
+    url: "http://live.adultiptv.net/fetish.m3u8",
+    tags: "XXX, Fetish",
+    desc: "Enjoy premium Fetish content streaming at high speed."
+  },
+  {
+    title: "AdultIPTV.net Pornstar",
+    thumbnail: "https://files.adultiptv.net/adultiptvnet.jpg",
+    url: "http://live.adultiptv.net/pornstar.m3u8",
+    tags: "XXX, Pornstar",
+    desc: "Enjoy premium Pornstar content streaming at high speed."
+  },
+  {
+    title: "AdultIPTV.net Big Ass",
+    thumbnail: "https://files.adultiptv.net/adultiptvnet.jpg",
+    url: "http://live.adultiptv.net/bigass.m3u8",
+    tags: "XXX, Big Ass",
+    desc: "Enjoy premium Big Ass content streaming at high speed."
+  },
+  {
+    title: "AdultIPTV.net Interracial",
+    thumbnail: "https://files.adultiptv.net/adultiptvnet.jpg",
+    url: "http://live.adultiptv.net/interracial.m3u8",
+    tags: "XXX, Interracial",
+    desc: "Enjoy premium Interracial content streaming at high speed."
+  },
+  {
+    title: "AdultIPTV.net Latina",
+    thumbnail: "https://files.adultiptv.net/adultiptvnet.jpg",
+    url: "http://live.adultiptv.net/latina.m3u8",
+    tags: "XXX, Latina",
+    desc: "Enjoy premium Latina content streaming at high speed."
+  },
+  {
+    title: "AdultIPTV.net POV",
+    thumbnail: "https://files.adultiptv.net/adultiptvnet.jpg",
+    url: "http://live.adultiptv.net/pov.m3u8",
+    tags: "XXX, POV",
+    desc: "Enjoy premium POV content streaming at high speed."
+  },
+  {
+    title: "AdultIPTV.net Blowjob",
+    thumbnail: "https://files.adultiptv.net/adultiptvnet.jpg",
+    url: "http://live.adultiptv.net/blowjob.m3u8",
+    tags: "XXX, Blowjob",
+    desc: "Enjoy premium Blowjob content streaming at high speed."
+  },
+  {
+    title: "AdultIPTV.net Hardcore",
+    thumbnail: "https://files.adultiptv.net/adultiptvnet.jpg",
+    url: "http://live.adultiptv.net/hardcore.m3u8",
+    tags: "XXX, Hardcore",
+    desc: "Enjoy premium Hardcore content streaming at high speed."
+  },
+  {
+    title: "AdultIPTV.net Cuckold",
+    thumbnail: "https://files.adultiptv.net/adultiptvnet.jpg",
+    url: "http://live.adultiptv.net/cuckold.m3u8",
+    tags: "XXX, Cuckold",
+    desc: "Enjoy premium Cuckold content streaming at high speed."
+  },
+  {
+    title: "AdultIPTV.net Threesome",
+    thumbnail: "https://files.adultiptv.net/adultiptvnet.jpg",
+    url: "http://live.adultiptv.net/threesome.m3u8",
+    tags: "XXX, Threesome",
+    desc: "Enjoy premium Threesome content streaming at high speed."
+  },
+  {
+    title: "AdultIPTV.net Russian",
+    thumbnail: "https://files.adultiptv.net/adultiptvnet.jpg",
+    url: "http://live.adultiptv.net/russian.m3u8",
+    tags: "XXX, Russian",
+    desc: "Enjoy premium Russian content streaming at high speed."
+  },
+  {
+    title: "AdultIPTV.net Lesbian",
+    thumbnail: "https://files.adultiptv.net/adultiptvnet.jpg",
+    url: "http://live.adultiptv.net/lesbian.m3u8",
+    tags: "XXX, Lesbian",
+    desc: "Enjoy premium Lesbian content streaming at high speed."
+  },
+  {
+    title: "AdultIPTV.net Rough",
+    thumbnail: "https://files.adultiptv.net/adultiptvnet.jpg",
+    url: "http://live.adultiptv.net/rough.m3u8",
+    tags: "XXX, Rough",
+    desc: "Enjoy premium Rough content streaming at high speed."
+  },
+  {
+    title: "AdultIPTV.net Gangbang",
+    thumbnail: "https://files.adultiptv.net/adultiptvnet.jpg",
+    url: "http://live.adultiptv.net/gangbang.m3u8",
+    tags: "XXX, Gangbang",
+    desc: "Enjoy premium Gangbang content streaming at high speed."
+  },
+  {
+    title: "AdultIPTV.net Anal",
+    thumbnail: "https://files.adultiptv.net/adultiptvnet.jpg",
+    url: "http://live.adultiptv.net/anal.m3u8",
+    tags: "XXX, Anal",
+    desc: "Enjoy premium Anal content streaming at high speed."
+  },
+  {
+    title: "AdultIPTV.net Compilation",
+    thumbnail: "https://files.adultiptv.net/adultiptvnet.jpg",
+    url: "http://live.adultiptv.net/compilation.m3u8",
+    tags: "XXX, Compilation",
+    desc: "Enjoy premium Compilation content streaming at high speed."
+  },
+  {
+    title: "AdultIPTV.net Brunette",
+    thumbnail: "https://files.adultiptv.net/adultiptvnet.jpg",
+    url: "http://live.adultiptv.net/brunette.m3u8",
+    tags: "XXX, Brunette",
+    desc: "Enjoy premium Brunette content streaming at high speed."
+  },
+  {
+    title: "AdultIPTV.net Blonde",
+    thumbnail: "https://files.adultiptv.net/adultiptvnet.jpg",
+    url: "http://live.adultiptv.net/blonde.m3u8",
+    tags: "XXX, Blonde",
+    desc: "Enjoy premium Blonde content streaming at high speed."
+  },
+  {
+    title: "AdultIPTV.net Gay",
+    thumbnail: "https://files.adultiptv.net/adultiptvnet.jpg",
+    url: "http://live.adultiptv.net/gay.m3u8",
+    tags: "XXX, Gay",
+    desc: "Enjoy premium Gay content streaming at high speed."
+  },
+  {
+    title: "AdultIPTV.net Asian",
+    thumbnail: "https://files.adultiptv.net/adultiptvnet.jpg",
+    url: "http://live.adultiptv.net/asian.m3u8",
+    tags: "XXX, Asian",
+    desc: "Enjoy premium Asian content streaming at high speed."
+  }
 ];
